1f53e42a5da28dba98c5f2f523dc1d430d050eb3
Dgeison <paradg@gmail.com>
2021-02-20 00:42:20 -0300

    até aqui remodelamos todos os arquivos em pastas

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,6 +1,5 @@
-const fs = require("fs");
-const data = require("../data.json");
 const { age, date } = require("../utils");
+
 
 exports.index = function (req, res) {
   return res.render("instructors/index", { instructors: data.instructors });
