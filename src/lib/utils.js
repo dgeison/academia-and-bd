0528f43fbbb8abbd89263b4666dc2be6221e2fac
Dgeison <paradg@gmail.com>
2021-02-20 01:03:45 -0300

    shorthand na pasta lib

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-  age: function age(timestamp) {
+  age(timestamp) {
     const today = new Date();
     const birthDate = new Date(timestamp);
 
@@ -14,7 +14,7 @@ module.exports = {
     return age;
   },
 
-  date: function (timestamp) {
+  date(timestamp) {
     const date = new Date(timestamp);
 
     const year = date.getUTCFullYear();
@@ -28,7 +28,7 @@ module.exports = {
       month,
       year,
       iso: `${year}-${month}-${day}`,
-      birthDay: `${day}/${month}`
+      birthDay: `${day}/${month}`,
     };
   },
 };
