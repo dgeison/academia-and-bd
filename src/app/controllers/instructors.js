e026420cd8ae7b6c190955d2bbe550656f23372f
Dgeison <paradg@gmail.com>
2021-02-20 01:50:34 -0300

    atualizado o banco e capturado o direcionamento

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
-const { age, date } = require("../../lib/utils");
+const { date } = require("../../lib/utils");
+const db = require("../../config/db");
 
 module.exports = {
   index(req, res) {
@@ -18,9 +19,27 @@ module.exports = {
       }
     }
 
-    let { avatar_URL, birth, name, services, gender } = req.body;
+    const query = `
+      INSERT INTO instructors (
+        name, avatar_URL, gender, services, birth, created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `;
 
-    return;
+    const values = [
+      req.body.name,
+      req.body.avatar_URL,
+      req.body.gender,
+      req.body.services,
+      date(req.body.birth).iso,
+      date(Date.now()).iso,
+    ];
+
+    db.query(query, values, function (err, results) {
+      if(err) return res.send('Database Error!')
+
+      return res.redirect(`instructors/${results.rows[0].id}`);
+    });
   },
 
   show(req, res) {
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,7 +7,9 @@ module.exports = {
 
     const month = today.getMonth() - birthDate.getMonth();
 
-    if (month < 0 || (month == 0 && today.getDate() < birthDate.getDate())) {
+    if (month < 0 || 
+        month == 0 && 
+        today.getDate() <= birthDate.getDate()) {
       age = age - 1;
     }
 
