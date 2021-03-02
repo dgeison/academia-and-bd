9115c4498af58b4b1f36b48d3e528c7b2b89bb49
Dgeison <paradg@gmail.com>
2021-03-02 19:24:36 -0300

    apagando a function filter

--- public/scripts.js
+++ public/scripts.js
@@ -28,29 +28,32 @@ if (formDelete) {
 // selectedPage = 15
 // [1, ..., 13, 14, 15, 16, 17,...,20]
 
-let totalPages = 20,
-  selectedPage = 5,
-  pages = [],
-  oldPage;
+function pagination(selectedPage, totalPages) {
+  let pages = [],
+      oldPage;
 
-for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
-  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
-  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
 
-  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
-    if (oldPage && currentPage - oldPage > 2) {
-      pages.push("...");
+    if (
+      firstAndLastPage ||
+      (pagesBeforeSelectedPage && pagesAfterSelectedPage)
+    ) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...");
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1);
+      }
+
+      pages.push(currentPage);
+
+      oldPage = currentPage;
     }
-
-    if(oldPage && currentPage - oldPage == 2){
-      pages.push(oldPage + 1);
-    }
-
-    pages.push(currentPage);
-
-    oldPage = currentPage;
   }
-}
 
-console.log(pages);
+  return pages;
+}
