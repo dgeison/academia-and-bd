08e8190cbded6b9fe04ec8945919f1f814e56b5c
Dgeison <paradg@gmail.com>
2021-03-02 14:26:58 -0300

    relacionamentos entre tabelas

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,8 +1,13 @@
 const { age, date } = require("../../lib/utils");
 
+const Member = require("../models/Member");
+
+
 module.exports = {
   index(req, res) {
-    return res.render("members/index");
+    Member.all(function (members) {
+      return res.render("members/index", { members });
+    });
   },
 
   create(req, res) {
@@ -17,15 +22,31 @@ module.exports = {
         return res.send("Please, fill all fields!");
       }
     }
-    return;
+
+    Member.create(req.body, function (member) {
+      return res.redirect(`/members/${member.id}`);
+    });
   },
 
   show(req, res) {
-    return;
+    //params
+    Member.find(req.params.id, function (member) {
+      if (!member) res.send("Member not found!");
+
+      member.birth = date(member.birth).birthDay;
+
+      return res.render("members/show", { member });
+    });
   },
 
   edit(req, res) {
-    return;
+    Member.find(req.params.id, function (member) {
+      if (!member) res.send("Member not found!");
+
+      member.birth = date(member.birth).iso;
+
+      return res.render("members/edit", { member });
+    });
   },
 
   put(req, res) {
@@ -37,10 +58,14 @@ module.exports = {
       }
     }
 
-    return;
+    Member.update(req.body, function(){
+      return res.redirect(`/members/${req.body.id}`)
+    })
   },
 
   delete(req, res) {
-    return;
+    Member.delete(req.body.id, function(){
+      return res.redirect(`/members`)
+    })
   },
 };
