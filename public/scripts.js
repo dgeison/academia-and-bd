4291f5e0f7017c542783101e42964f28c920164e
Dgeison <paradg@gmail.com>
2021-03-02 19:21:00 -0300

    insert function paginate

--- public/scripts.js
+++ public/scripts.js
@@ -13,10 +13,44 @@ console.log(currentPage);
 const formDelete = document.querySelector("#form-delete");
 
 if (formDelete) {
-  currentPage.includes(formDelete.addEventListener("submit", function (event) {
-    const confirmation = confirm("Deseja deletar?");
-    if (!confirmation) {
-      event.preventDefault();
-    }
-  }));
+  currentPage.includes(
+    formDelete.addEventListener("submit", function (event) {
+      const confirmation = confirm("Deseja deletar?");
+      if (!confirmation) {
+        event.preventDefault();
+      }
+    })
+  );
 }
+
+// Paginação
+// totalPages = 20
+// selectedPage = 15
+// [1, ..., 13, 14, 15, 16, 17,...,20]
+
+let totalPages = 20,
+  selectedPage = 5,
+  pages = [],
+  oldPage;
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...");
+    }
+
+    if(oldPage && currentPage - oldPage == 2){
+      pages.push(oldPage + 1);
+    }
+
+    pages.push(currentPage);
+
+    oldPage = currentPage;
+  }
+}
+
+console.log(pages);
