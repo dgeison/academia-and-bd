eef50542d1fe65165c0ee2f1742ad9e6a4063220
Dgeison <paradg@gmail.com>
2021-03-02 22:29:27 -0300

    concluído paginação de membros

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -106,6 +106,7 @@ module.exports = {
       }
     );
   },
+
   instructorsSelectOptions(callback) {
     db.query(`SELECT name, id FROM instructors`, function (err, results) {
       if (err) throw "Database Error!";
@@ -113,4 +114,40 @@ module.exports = {
       callback(results.rows);
     });
   },
+
+  paginate(params) {
+    const { filter, limit, offset, callback } = params;
+
+    let query = "",
+      filterQuery = "",
+      totalQuery = `(
+          SELECT count(*) FROM members
+          ) AS total`;
+
+    if (filter) {
+      filterQuery = `${query}
+      WHERE members.name ILIKE '%${filter}%'
+      OR members.email ILIKE '%${filter}%'
+      `;
+
+      totalQuery = `(
+        SELECT count(*) FROM members
+        ${filterQuery}
+      ) AS total`;
+    }
+
+    query = `
+    SELECT members.*, ${totalQuery}
+    FROM members
+    ${filterQuery}
+    LIMIT $1 OFFSET $2
+    `;
+
+    db.query(query, [limit, offset], function (err, results) {
+      if (err) throw "Database Error!";
+
+      callback(results.rows);
+    });
+  },
+
 };
